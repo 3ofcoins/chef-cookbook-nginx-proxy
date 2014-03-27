ad948c8bce6a154f209981caf1c92c0f2f4c18b0
Maciej Pasternacki <maciej@3ofcoins.net>
2014-03-27 11:50:23 +0100

    depend on apache, it makes behaviour less surprising

--- metadata.rb
+++ metadata.rb
@@ -8,5 +8,5 @@ description      'Nginx Proxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'apache2'
 depends 'nginx'
-suggests 'apache2'
--- spec/apache2_spec.rb
+++ spec/apache2_spec.rb
@@ -3,11 +3,10 @@
 require_relative './spec_helper'
 
 describe 'nginx-proxy::apache2' do
-  let(:run_list) { ['apache2', described_recipe]}
   let(:chef_run) { ChefSpec::Runner.new }
 
   it 'should install Apache and have it listen on a non-standard port' do
-    chef_run.converge(*run_list)
+    chef_run.converge(described_recipe)
     expect(chef_run).to include_recipe('nginx-proxy::default')
     expect(chef_run).to include_recipe('nginx::default')
     expect(chef_run).to include_recipe('apache2::default')
@@ -17,7 +16,7 @@ describe 'nginx-proxy::apache2' do
 
   it "can override apache's non-standard port via attribute" do
     chef_run.node.set['nginx_proxy']['apache_port'] = 88
-    chef_run.converge(*run_list)
+    chef_run.converge(described_recipe)
     expect(chef_run.node['apache']['listen_ports']).to be == [88]
     expect(chef_run.node['apache']['listen_addresses']).to be == %w[127.0.0.1]
   end
